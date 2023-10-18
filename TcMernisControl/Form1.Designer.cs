5ad22d85d089cf6cbd4334d1319798600c64e691
selimmemduhoglu <mselimmemduhoglu@gmail.com>
2023-10-18 11:58:24 +0300

    Add project files.

---
+++ TcMernisControl/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace TcMernisControl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtTC = new System.Windows.Forms.TextBox();
+            this.txtDate = new System.Windows.Forms.TextBox();
+            this.txtSurname = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(125, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "TC";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(125, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(126, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Surname";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(125, 187);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Date";
+            // 
+            // txtTC
+            // 
+            this.txtTC.Location = new System.Drawing.Point(200, 57);
+            this.txtTC.Name = "txtTC";
+            this.txtTC.Size = new System.Drawing.Size(151, 22);
+            this.txtTC.TabIndex = 4;
+            // 
+            // txtDate
+            // 
+            this.txtDate.Location = new System.Drawing.Point(200, 187);
+            this.txtDate.Name = "txtDate";
+            this.txtDate.Size = new System.Drawing.Size(151, 22);
+            this.txtDate.TabIndex = 5;
+            // 
+            // txtSurname
+            // 
+            this.txtSurname.Location = new System.Drawing.Point(200, 141);
+            this.txtSurname.Name = "txtSurname";
+            this.txtSurname.Size = new System.Drawing.Size(151, 22);
+            this.txtSurname.TabIndex = 6;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(200, 96);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(151, 22);
+            this.txtName.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Location = new System.Drawing.Point(200, 230);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 41);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "SAVE";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtSurname);
+            this.Controls.Add(this.txtDate);
+            this.Controls.Add(this.txtTC);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtTC;
+        private System.Windows.Forms.TextBox txtDate;
+        private System.Windows.Forms.TextBox txtSurname;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ TcMernisControl/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace TcMernisControl
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            long kimlikNo = long.Parse(txtTC.Text);
+            int yil = int.Parse(txtDate.Text);
+
+            bool durum;
+
+            try
+            {
+                using (TCMernis.KPSPublicSoapClient service = new TCMernis.KPSPublicSoapClient { })
+                {
+                    durum = service.TCKimlikNoDogrula(kimlikNo, txtName.Text, txtSurname.Text, yil);
+
+                }
+
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            MessageBox.Show(durum.ToString());
+        }
+    }
+}
